2ce8b96afdec4d46db424f1d2f5130ccb28a7380
Serhii Bets <serhii.bets@capgemini.com>
2023-01-06 10:36:03 +0100

    Add WeatherModel

---
+++ OpenWeatherApp/OpenWeatherApp/WeatherModel.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherModel.swift
+//  OpenWeatherApp
+//
+//  Created by Serhii Bets on 6/1/23.
+//
+
+import Foundation
+
+struct WeatherModel: Decodable{
+    let lat, lon: Double
+    let current: Current
+    let hourly: [HourlyWeather]
+    let daily: [DailyWeather]
+}
+
+struct Current: Decodable {
+    let dt, temp: Double
+    let weather: [WeatherInfo]
+}
+
+struct WeatherInfo: Decodable {
+    let description, icon: String
+}
+
+struct HourlyWeather: Decodable{
+    let dt, temp: Double
+    let weather: [HourlyWeatherInfo]
+}
+
+struct HourlyWeatherInfo: Decodable{
+    let description, icon: String
+}
+
+struct DailyWeather: Decodable{
+    let dt: Double
+    let temp: DailyTemp
+    let weather: [DailyWeatherInfo]
+}
+
+struct DailyTemp: Decodable{
+    let min, max: Double
+}
+
+struct DailyWeatherInfo: Decodable{
+    let icon: String
+}
