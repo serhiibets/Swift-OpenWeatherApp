a1804b40888392bd978d6c5e45b1705e72f26d3d
Serhii Bets <serhii.bets@capgemini.com>
2023-01-06 10:33:21 +0100

    Create and config project

---
+++ OpenWeatherApp/OpenWeatherApp/WeatherViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Serhii Bets on 5/1/23.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+    
+}
+
