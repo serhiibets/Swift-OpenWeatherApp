29f78ad2c344ed93dce418c478fd901da92600c8
Serhii Bets <serhii.bets@capgemini.com>
2023-01-06 23:25:05 +0100

    Add Presenter and Interactor to WeatherViewController

--- OpenWeatherApp/OpenWeatherApp/WeatherViewController.swift
+++ OpenWeatherApp/OpenWeatherApp/WeatherViewController.swift
@@ -8,17 +8,53 @@
 import UIKit
 import CoreLocation
 
-class WeatherViewController: UIViewController {
+protocol WeatherDisplayLogic: AnyObject {
+    func displayData(viewModel: WeatherEnumModel.ViewModel.ViewModelData)
+}
+
+class WeatherViewController: UIViewController, WeatherDisplayLogic {
     let weatherView = WeatherView()
+    var interactor: WeatherBusinessLogic?
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        let viewController        = self
+        let interactor            = WeatherInteractor()
+        let presenter             = WeatherPresenter()
+        viewController.interactor = interactor
+        interactor.presenter      = presenter
+        presenter.viewController  = viewController
+    }
     
     // MARK: - View lifecycle
-    override func loadView() {
-        view = weatherView
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadView()
+        setup()
+    
+        view.addSubview(weatherView)
+        weatherView.frame = self.view.frame
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        interactor?.makeRequest(request: .getWeather)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        weatherView.contentSize = CGSize(width:self.view.bounds.width, height: 845)
+    }
+    
+    //MARK: - displayData
+    func displayData(viewModel: WeatherEnumModel.ViewModel.ViewModelData) {
+        
+        switch viewModel {
+
+        case .displayWeather(let currentWeatherViewModel):
+            weatherView.configure(viewModel: currentWeatherViewModel)
+        }
     }
 }
 
