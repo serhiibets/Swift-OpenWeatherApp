5dc1ec99d6767a43cf22d0a61ecfd3d9d489ac57
Serhii Bets <serhii.bets@capgemini.com>
2023-01-09 23:50:47 +0100

    Change URLSession to Alamofire

--- OpenWeatherApp/OpenWeatherApp/Services/NetworkService.swift
+++ OpenWeatherApp/OpenWeatherApp/Services/NetworkService.swift
@@ -4,24 +4,21 @@
 //  Created by Serhii Bets on 6/1/23.
 //
 import Foundation
+import Alamofire
 
 protocol NetworkServiceProtocol{
     func getWeather(coordinates: String, completion: @escaping (WeatherModel?) -> Void)
 }
 
-struct NetworkService: NetworkServiceProtocol{
+struct NetworkService: NetworkServiceProtocol{    
     func getWeather(coordinates: String, completion: @escaping (WeatherModel?) -> Void) {
         let fullUrl = "\(API.url)\(API.apiKey)&\(coordinates)"
         guard let url = URL(string: fullUrl) else { return }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Error received requesting data: \(error.localizedDescription)")
-                completion(nil)
-            }
+        let _ = AF.request(url, method: .get).validate().response { response in
+            guard let data = response.data else { return }
             let decoded = self.decodeJSON(type: WeatherModel.self, from: data)
             completion(decoded)
         }
-        .resume()
     }
     
     private func decodeJSON<T: Decodable>(type: T.Type, from: Data?) -> T?{
