a4706288bef1a3e4162cb30c7ada40202dd2245e
Serhii Bets <serhii.bets@capgemini.com>
2023-01-09 17:15:39 +0100

    Add colors to Assets

--- OpenWeatherApp/OpenWeatherApp/Helpers/AppStyle.swift
+++ OpenWeatherApp/OpenWeatherApp/Helpers/AppStyle.swift
@@ -7,17 +7,16 @@
 
 import UIKit
 
-enum AppStyle {
-    struct light {
-        static let primaryBackgroundColor     = #colorLiteral(red: 0.2876678407, green: 0.5634036064, blue: 0.88738662, alpha: 1)
-        static let secondaryBackgroundColor   = #colorLiteral(red: 0.3562327027, green: 0.6222828627, blue: 0.9425352216, alpha: 1)
-        static let whiteBackgroundColor       = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        static let blackBackgroundColor       = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        static let whiteTextColor             = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        static let blackTextColor             = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-    }
+enum AssetsColor {
+    case primaryBackground
+    case secondaryBackground
+    case whiteBackground
     
-    struct dark {
-        //TODO: implement dark mode style
+    var color: UIColor {
+        switch self {
+            case .primaryBackground   : return UIColor(named: "AppStylePrimaryBackgroundColor")!
+            case .secondaryBackground : return UIColor(named: "AppStyleSecondaryBackgroundColor")!
+            case .whiteBackground     : return UIColor.white
+        }
     }
 }
