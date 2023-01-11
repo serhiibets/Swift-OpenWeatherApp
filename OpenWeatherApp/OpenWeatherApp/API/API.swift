a935a74783f9b94244e8192f5e2a89d921501392
Serhii Bets <serhii.bets@capgemini.com>
2023-01-11 10:24:46 +0100

    Add API.swift

---
+++ OpenWeatherApp/OpenWeatherApp/API/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  OpenWeatherApp
+//
+//  Created by Serhii Bets on 6/1/23.
+//
+
+import Foundation
+
+struct API{
+    static let apiKey = "-"
+    static let url = "https://api.openweathermap.org/data/3.0/onecall?lang=ru&minutely&units=metric&appid="
+}
