5b4abd66c84128ce7a7f1e86792cd3a89ea13ca8
Serhii Bets <serhii.bets@capgemini.com>
2023-01-11 12:48:37 +0100

    Fixed background color

--- OpenWeatherApp/OpenWeatherApp/API/API.swift
+++ OpenWeatherApp/OpenWeatherApp/API/API.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct API{
-    static let apiKey = "-"
+    static let apiKey = "e9fa299de8a9712b970c909c001cac86"
     static let url = "https://api.openweathermap.org/data/3.0/onecall?lang=ru&minutely&units=metric&appid="
 }
--- OpenWeatherApp/OpenWeatherApp/ViewControllers/Views/MainScrollView.swift
+++ OpenWeatherApp/OpenWeatherApp/ViewControllers/Views/MainScrollView.swift
@@ -88,6 +88,6 @@ class MainScrollView: UIScrollView {
         self.contentInsetAdjustmentBehavior = .never
         self.showsVerticalScrollIndicator = false
         self.showsHorizontalScrollIndicator = false
-        backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        backgroundColor = AssetsColor.primaryBackground.color
     }
 }
